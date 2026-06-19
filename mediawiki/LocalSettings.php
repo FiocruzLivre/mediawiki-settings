abdc90d546b482168b8c1933c2d05254b04d7267
Lívia Gouvêa <livia@teclivre.com.br>
2026-06-19 15:21:10 -0300

    Atualiza extensão EmbedVideo

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -199,7 +199,7 @@ wfLoadExtension( 'FontAwesome' );
 wfLoadExtension( 'GeoData' );
 
 # Exibir vídeos
-wfLoadExtension( 'EmbedVideo' );
+wfLoadExtension( 'Embedvideo' );
 $wgEmbedVideoAddFileExtensions = true;
 $wgEmbedVideoEnableVideoHandler = true;
 $wgEmbedVideoEnableAudioHandler = true;
